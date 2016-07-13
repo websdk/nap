51b0c567962f4e7957308b706846d7608f288a25
Marcus Stade <marcus@stade.se>
2016-07-13 11:15:35 +0200

    Revert "Upgrading to rhumb 0.3.9."

--- test/uri.test.js
+++ test/uri.test.js
@@ -2,13 +2,11 @@ var test = require('tape')
   , nap  = require('../src/nap')
 
 test('Simple URI patterns', function(t) {
-  t.plan(4)
+  t.plan(2)
   var web = nap.web().resource('demo', '/my-demo', function() {})
 
   t.equal(web.uri('demo'), '/my-demo', 'should generate a URI based on resource name')
   t.equal(web.uri('/my-demo'), '/my-demo', 'should generate a URI based on resource path')
-  t.equal(web.uri('/'), '/', 'should generate a URI based on root resource path')
-  t.equal(web.uri(''), '/', 'should generate a URI based on empty resource path')
 })
 
 test('Complex URI patterns', function(t) {
